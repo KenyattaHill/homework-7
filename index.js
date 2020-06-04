d000c1ff79de7bb23cfbe3b3d4a03fea86f85bea
Kenyatta Hill <khill@insight2profit.com>
2020-06-04 01:00:19 -0400

    Done

--- index.js
+++ index.js
@@ -16,6 +16,51 @@ const questions = [
     message: 'Please enter a description.',
     default: 'TODO add description'
   },
+  {
+    type: 'confirm',
+    name: 'installation',
+    message: 'Do you want an installation section?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'usage',
+    message: 'Do you want a usage section?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'license',
+    message: 'Do you want a license section?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'contributing',
+    message: 'Do you want a contributing section?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'tests',
+    message: 'Do you want a tests section?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'tableOfContents',
+    message: 'Do you want a table of contents?'
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Please enter your github username'
+  },
+  {
+    type: 'input',
+    name: 'repository',
+    message: 'Please enter the name of your Github repository'
+  },
   
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,19 @@
 function generateMarkdown(data) {
+  const ignoredSection = type => ['title', 'description', 'tableOfContents', 'username', 'repository'].includes(type) || !data[type];
+  const titleCase = ([first, ...rest]) => first.toUpperCase() + rest.join('');
+  const keys = Object.keys(data).filter(key => !ignoredSection(key));
+  const tableOfContents = data.tableOfContents ? '\n## Table Of Contents\n\n' + keys.map(key => `* [${titleCase(key)}](#${key})`).join('\n') : '\n\n'
+  const sections = keys.map(key => `## ${titleCase(key)}\n\nTODO Add ${key} information`).join('\n\n')
   return `
+![badmath](https://img.shields.io/github/last-commit/${data.username}/${data.repository})
 # ${data.title}
 
 ## Description
+
 ${data.description}
+${tableOfContents}
 
-## Installation
-${data.installation}
-
+${sections}
 `;
 }
 
